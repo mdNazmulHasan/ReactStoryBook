cec9813d0937bdd46ae28a9b48b1ad79c03e160b
mdNazmulHasan <najmul.hasan10@gmail.com>
2019-05-07 10:23:03 +0600

    init commit

---
+++ src/ContextProject/home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Home = () => {
+  return (
+    <div
+      style={{ height: "200px", width: "400px", textAlign: "center", backgroundColor: "yellow" }}
+    >
+      <h1>This is my home page</h1>
+    </div>
+  );
+};
---
+++ src/ContextProject/home.stories.js
@@ -0,0 +1,4 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { Home } from "./home";
+storiesOf("Context api project", module).add("Home", () => <Home />);
